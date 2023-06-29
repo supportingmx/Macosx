c8a77fbdd93c90674d4119f2166c1fc3a17d8622
supportingmx <88459207+supportingmx@users.noreply.github.com>
2023-06-29 13:39:34 -0600

    Create eraser.sh

---
+++ eraser.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+sudo rm -rf /Users/v1boss/Library/Application\ Support/AddressBook/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/CallHistoryDB/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/CallHistoryTransactions/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/CloudDocs/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/com.apple.TCC/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/CrashReporter/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/Firefox/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/Google/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/Mozilla/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/Quick\ Look/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/Steam/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/BraveSoftware/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/Caches/
+sudo rm -rf /Users/v1boss/Library/Application\ Support/Vivaldi/
+sudo rm -rf /Users/v1boss/Library/Assistant/
+sudo rm -rf /Users/v1boss/Library/Containers/com.apple.Safari/
+sudo rm -rf /Users/v1boss/Library/Containers/com.apple.Safari.CacheDeleteExtension/
+sudo rm -rf /Users/v1boss/Library/Google/
+sudo rm -rf /Users/v1boss/Library/Caches/
+sudo rm -rf /Users/v1boss/Library/Cookies/
+sudo rm -rf /Users/v1boss/Library/Google/
+sudo rm -rf /Users/v1boss/Library/Vivaldi/
+sudo rm -rf /Users/v1boss/Library/Logs/
+sudo rm -rf /Users/v1boss/Library/Safari/
+sudo rm -rf /Users/v1boss/Library/Favoritos/
+sudo rm -rf /Users/v1boss/Library/Saved\ Application\ State/
+sudo rm -rf /Users/v1boss/Library/SafariSafeBrowsing/
+sudo rm -rf /Users/v1boss/Library/WebKit/org.mozilla.firefox
+sudo rm -rf /private/var/db/diagnostics/
+sudo rm -rf /Library/Logs/DiagnosticReports/
+sudo rm -rf /private/var/log/DiagnosticMessages/
+sudo rm -rf /private/var/log/asl/
+sudo rm -rf /private/var/log/
+sudo rm -rf /private/var/log/system.log
+sudo rm -rf /private/var/log/install.log
+sudo rm -rf /Users/v1boss/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2
+sudo rm -rf /Users/v1boss/Library/LaunchAgents/
+sudo rm -rf /private/var/folders/w0/t9pg6l4x7px774dggq63kh1h0000gp/C/com.apple.Safari/com.apple.Safari/
+sudo rm -rf /private/var/folders/w0/t9pg6l4x7px774dggq63kh1h0000gp/C/com.google.Chrome/
+sudo rm -rf /Users/v1boss/.Trash/
